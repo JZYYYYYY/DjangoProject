45804c67b3794182badefd0e0eb51e9ce97cc898
zy j <2687364286@qq.com>
2023-04-25 16:20:43 +0800

    add game_interface

--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from game.views import index
+from game.views import index,game_interface
 urlpatterns = [
-    path("",index,name="game index"),
+    path("",index,name="index"),
+    path("game/",game_interface,name="interface"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 
 def index(request):
-    line1 = '<h1 style="text-align: center">术士之战!</h1>'
-    line2 = '<img src="https://bkimg.cdn.bcebos.com/pic/c2cec3fdfc039245efdd58288f94a4c27d1e25a8?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U5Mg==,g_7,xp_5,yp_5" width=2000>'
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line3 = '<a href="/game">进入游戏界面</a>'
+    line2 = '<img src="https://bkimg.cdn.bcebos.com/pic/c2cec3fdfc039245efdd58288f94a4c27d1e25a8?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U5Mg==,g_7,xp_5,yp_5" width=800>'
+    return HttpResponse(line1+line3+line2)
+
+def game_interface(request):
+    line1 = '<h1 style="test-align: center">游戏界面</h1>'
+    line2 = '<a href="/">返回</a>'
     return HttpResponse(line1+line2)
