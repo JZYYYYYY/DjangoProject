c8c3189fe6c5e26a245b6a5ad1c7636ff1ecc50e
zy j <2687364286@qq.com>
2023-04-25 15:04:57 +0800

    add title picture

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战!</h1>'
+    line2 = '<img src="https://bkimg.cdn.bcebos.com/pic/c2cec3fdfc039245efdd58288f94a4c27d1e25a8?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U5Mg==,g_7,xp_5,yp_5" width=2000>'
+    return HttpResponse(line1+line2)
