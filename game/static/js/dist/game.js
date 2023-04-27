3636789d209df25efd086062db62f47658c8c9be
zy j <2687364286@qq.com>
2023-04-27 12:41:34 +0800

    add is_me

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -127,6 +127,76 @@ class GameMap extends AcGameObject{
         this.ctx.fillRect(0,0,this.ctx.canvas.width,this.ctx.canvas.height);
     }
 }
+class Player extends AcGameObject{
+    constructor(playground,x,y,redius,color,speed,is_me){
+        super();
+        this.playground=playground;
+        this.ctx=this.playground.game_map.ctx;
+        this.x=x;
+        this.y=y;
+        this.vx=0;
+        this.vy=0;
+        this.redius=redius;
+        this.color=color;
+        this.speed=speed;
+        this.is_me=is_me;
+        this.eps=0.1;
+        this.move_length=0;
+    }
+    
+    start(){
+        if(this.is_me){
+            this.add_listening_events();
+        }
+    }
+
+    add_listening_events(){
+        let outer=this;
+        this.playground.game_map.$canvas.on("contextmenu",function(){
+            return false;
+        })
+        this.playground.game_map.$canvas.mousedown(function(e){
+            if(e.which===3){
+                outer.move_to(e.clientX,e.clientY);
+            }
+        })
+    }
+
+    get_dist(x1,y1,x2,y2){
+        let dx=x1-x2;
+        let dy=y1-y2;
+        return Math.sqrt(dx*dx+dy*dy);
+    }
+
+    move_to(tx,ty){
+        this.move_length=this.get_dist(this.x,this.y,tx,ty);
+        let angle=Math.atan2(ty-this.y,tx-this.x);
+        this.vx=Math.cos(angle);
+        this.vy=Math.sin(angle);
+    }
+
+    update(){
+        if(this.move_length<this.eps){
+            this.move_length=0;
+            this.vx=0;
+            this.vy=0;
+        }
+        else{
+            let moved=Math.min(this.move_length,this.speed*this.timedelta/1000);
+            this.x+=this.vx*moved;
+            this.y+=this.vy*moved;
+            this.move_length-=moved;
+        }
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x,this.y,this.redius,0,Math.PI*2,false);
+        this.ctx.fillStyle=this.color;
+        this.ctx.fill();
+    }
+}
 class AcGamePlayground{
     constructor(root){
         this.root=root;
@@ -136,6 +206,9 @@ class AcGamePlayground{
         this.width=this.$playground.width();
         this.height=this.$playground.height();
         this.game_map=new GameMap(this);
+
+        this.players=[];
+        this.players.push(new Player(this,this.width/2,this.height/2,this.height*0.05,"white",this.height*0.15,true));
         this.start();
 
     }
